2e3832319a48347187d50d2e233887736e59044a
Unknown <Kizary-archer@yandex.ru>
2018-11-13 23:50:44 +0300

    decryption

---
+++ src/backpack/decryption.java
@@ -0,0 +1,26 @@
+package backpack;
+
+final class decryption {
+ int[] EncMessage;
+
+		public void Getdecryption(int[] Key,String str) {
+			char[] CharArray = str.toCharArray();
+			EncMessage = new int[CharArray.length];
+			int res;
+			int j = 0 ;
+			for (int valChar : CharArray) {
+				 System.out.println(Integer.toBinaryString(valChar));
+				 System.out.println(valChar);
+				 res = 0;
+			int i = Key.length-1;
+			for (int valKey : Key) {
+				 if((valChar & (int)Math.pow(2,i))>0)
+				  res += valKey;
+				  i--;
+			}
+			System.out.println(res);
+			EncMessage[j] = res;
+			j++;
+				}
+	}
+}
